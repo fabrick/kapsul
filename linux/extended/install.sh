3c97e437498e66e385b9f45e395c318ce2c1ec8e
Damien Charon <damien.charon@fabrick.io>
2021-06-09 15:04:27 +0200

    Add Grafana package signature

--- linux/extended/install.sh
+++ linux/extended/install.sh
@@ -57,6 +57,7 @@ then
 else
     apt-get install -y apt-transport-https
     apt-get install -y software-properties-common wget
+    wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
     echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
     apt-get update
     apt-get install grafana
