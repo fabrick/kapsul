8406867cd05ada12fcf5b4c5e0e2516da9d454da
Damien Charon <damien.charon@fabrick.io>
2021-10-19 09:57:52 +0200

    Add get java

--- linux/migrations/migration-v1.3-to-1.4.sh
+++ linux/migrations/migration-v1.3-to-1.4.sh
@@ -26,6 +26,31 @@ do
     esac
 done
 
+function getJava() {
+    if type -p java; then
+        echo found java executable in PATH
+        _java=$(which java)
+    elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+        echo found java executable in JAVA_HOME     
+        _java="$JAVA_HOME/bin/java"
+    else
+        echo "No java found. Please install a Java Runtime v1.8"
+        exit 101
+    fi
+
+    if [[ "$_java" ]]; then
+        fullversion=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+        version=${fullversion:0:3}
+        echo version "$fullversion"
+        if [[ "$version" = "1.8" ]]; then
+            JAVA_PATH=$_java
+        else         
+            echo "Version is different from version 1.8. Kapsul may not work"
+            JAVA_PATH=$_java
+        fi
+    fi
+}
+
 systemctl is-active --quiet kapsul
 if [ $? -eq 0 ]
 then
